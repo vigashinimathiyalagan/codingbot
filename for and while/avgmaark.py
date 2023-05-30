7005590cd2e83a8eac922d2454bfd147fc76c911
vigashinimathiyalagan <112497916+vigashinimathiyalagan@users.noreply.github.com>
2023-05-30 21:25:14 +0530

    Update avgmaark.py

--- for and while/avgmaark.py
+++ for and while/avgmaark.py
@@ -1,1 +1,22 @@
+total = 0
+noOfStudents = 5
+i = 0
+while i < noOfStudents:
+    mark = float(input("Enter the mark: "))
+    total += mark
+    i += 1
+
+average = total / noOfStudents
+print("Average is", average)
+
+output:
+  Output:
+Enter the mark: 67
+Enter the mark: 89
+Enter the mark: 89
+Enter the mark: 90
+Enter the mark: 90
+Average is 85.0
+
+
 
