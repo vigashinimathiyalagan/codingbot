3e88ff575654bb8e1e94cf7c4a76886ad88b9886
vigashinimathiyalagan <112497916+vigashinimathiyalagan@users.noreply.github.com>
2023-06-08 21:22:42 +0530

    Update piglatin.py

--- string/piglatin.py
+++ string/piglatin.py
@@ -1,1 +1,9 @@
-
+inputSentence = "I am Python"
+pigLatinKey = 'ay'
+for word in inputSentence.split():
+    firstChar = word[0]  # take the first character of the word
+    if len(word) > 1:  # check if the word has more than one character
+        word = word[1:] + firstChar + pigLatinKey
+    print(word, end=' ')
+output:
+  I maay ythonPay 
