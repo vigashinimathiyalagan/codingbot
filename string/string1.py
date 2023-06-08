3ae151def0445573553317c7f2d6d8de19d739ac
vigashinimathiyalagan <112497916+vigashinimathiyalagan@users.noreply.github.com>
2023-06-08 21:15:48 +0530

    Update string1.py

--- string/string1.py
+++ string/string1.py
@@ -1,1 +1,15 @@
+inputString = input("Enter a string: ")
+i = 0
+newString = ""
+
+while i < len(inputString):
+    newString += inputString[i:i+2]  
+    newString += " " 
+    i += 2
+
+
+if i < len(inputString):
+    newString += inputString[i:]
+
+print(newString)
 
