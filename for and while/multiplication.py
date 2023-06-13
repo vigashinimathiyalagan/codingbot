30fa9d2e361bc6247ce02e0a1efd1905b644d472
vigashinimathiyalagan <112497916+vigashinimathiyalagan@users.noreply.github.com>
2023-06-13 21:17:34 +0530

    Update multiplication.py

--- for and while/multiplication.py
+++ for and while/multiplication.py
@@ -1,1 +1,172 @@
+#problem 2
 
+for i in range(7, 17):
+    print(f"Multiplication Table of {i}:")
+    for j in range(1, 13):
+        print(f"{i} x {j} = {i * j}")
+    print()
+    output:
+      
+Multiplication Table of 7:
+7 x 1 = 7
+7 x 2 = 14
+7 x 3 = 21
+7 x 4 = 28
+7 x 5 = 35
+7 x 6 = 42
+7 x 7 = 49
+7 x 8 = 56
+7 x 9 = 63
+7 x 10 = 70
+7 x 11 = 77
+7 x 12 = 84
+
+Multiplication Table of 8:
+8 x 1 = 8
+8 x 2 = 16
+8 x 3 = 24
+8 x 4 = 32
+8 x 5 = 40
+8 x 6 = 48
+8 x 7 = 56
+8 x 8 = 64
+8 x 9 = 72
+8 x 10 = 80
+8 x 11 = 88
+8 x 12 = 96
+
+Multiplication Table of 9:
+9 x 1 = 9
+9 x 2 = 18
+9 x 3 = 27
+9 x 4 = 36
+9 x 5 = 45
+9 x 6 = 54
+9 x 7 = 63
+9 x 8 = 72
+9 x 9 = 81
+9 x 10 = 90
+9 x 11 = 99
+9 x 12 = 108
+
+Multiplication Table of 10:
+10 x 1 = 10
+10 x 2 = 20
+10 x 3 = 30
+10 x 4 = 40
+10 x 5 = 50
+10 x 6 = 60
+10 x 7 = 70
+10 x 8 = 80
+10 x 9 = 90
+10 x 10 = 100
+10 x 11 = 110
+10 x 12 = 120
+
+Multiplication Table of 11:
+11 x 1 = 11
+11 x 2 = 22
+11 x 3 = 33
+11 x 4 = 44
+11 x 5 = 55
+11 x 6 = 66
+11 x 7 = 77
+11 x 8 = 88
+11 x 9 = 99
+11 x 10 = 110
+11 x 11 = 121
+11 x 12 = 132
+
+Multiplication Table of 12:
+12 x 1 = 12
+12 x 2 = 24
+12 x 3 = 36
+12 x 4 = 48
+12 x 5 = 60
+12 x 6 = 72
+12 x 7 = 84
+12 x 8 = 96
+12 x 9 = 108
+12 x 10 = 120
+12 x 11 = 132
+12 x 12 = 144
+
+Multiplication Table of 13:
+13 x 1 = 13
+13 x 2 = 26
+13 x 3 = 39
+13 x 4 = 52
+13 x 5 = 65
+13 x 6 = 78
+13 x 7 = 91
+13 x 8 = 104
+13 x 9 = 117
+13 x 10 = 130
+13 x 11 = 143
+13 x 12 = 156
+
+Multiplication Table of 14:
+14 x 1 = 14
+14 x 2 = 28
+14 x 3 = 42
+14 x 4 = 56
+14 x 5 = 70
+14 x 6 = 84
+14 x 7 = 98
+14 x 8 = 112
+14 x 9 = 126
+14 x 10 = 140
+14 x 11 = 154
+14 x 12 = 168
+
+Multiplication Table of 15:
+15 x 1 = 15
+15 x 2 = 30
+15 x 3 = 45
+15 x 4 = 60
+15 x 5 = 75
+15 x 6 = 90
+15 x 7 = 105
+15 x 8 = 120
+15 x 9 = 135
+15 x 10 = 150
+15 x 11 = 165
+15 x 12 = 180
+
+Multiplication Table of 16:
+16 x 1 = 16
+16 x 2 = 32
+16 x 3 = 48
+16 x 4 = 64
+16 x 5 = 80
+16 x 6 = 96
+16 x 7 = 112
+16 x 8 = 128
+16 x 9 = 144
+16 x 10 = 160
+16 x 11 = 176
+16 x 12 = 192
+
+#problem 3
+chess board
+def print_chessboard():
+    for row in range(8):
+        for col in range(8):
+            if (row + col) % 2 == 0:
+                print('\u25A0', end=' ')  # Print black square
+            else:
+                print('B', end=' ')  # Print white square
+        print()  # Move to the next row
+
+print_chessboard()
+
+output:
+  
+  ■ B ■ B ■ B ■ B 
+B ■ B ■ B ■ B ■
+■ B ■ B ■ B ■ B
+B ■ B ■ B ■ B ■
+■ B ■ B ■ B ■ B
+B ■ B ■ B ■ B ■
+■ B ■ B ■ B ■ B
+B ■ B ■ B ■ B ■
